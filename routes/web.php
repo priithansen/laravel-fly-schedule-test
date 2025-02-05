ec0737ac64dc2c58cb98101b40c6412ee2ecb7c6
Priit <priit@hpr.ee>
2025-02-05 10:40:10 +0200

    Add test route to make sure we are seeing logs at all

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/test', function () {
+    Log::info('Test route is working fine!');
+    return 'test';
 });
